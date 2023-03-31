109fa957aa15328c5a4367829242b8a45f886d93
Crislanias <crislania.oficial@gmail.com>
2023-03-30 23:27:35 -0300

    blogpessoal atualização

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,5 +1,36 @@
 package com.generation.blogpessoal.repository;
+	
+import java.util.List;
 
-public interface PostagemRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-}
+import com.generation.blogpessoal.model.Postagem;
+
+
+	@Repository
+	public interface PostagemRepository extends JpaRepository< Postagem, Long> {
+	public abstract List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+
+	
+		}
+		
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo ) {
+	return ResponseeEntity.ok(postagemRepository.findAllbyTituloContainingIgnoreCase(titulo));
+
+	}
+
+	
+
+
+	
+	
+	
+	
+	
+	
